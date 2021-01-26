c0f2da3e9f071408a8a486524ebefe04e5336099
songsu73 <70936130+songsu73@users.noreply.github.com>
2021-01-26 14:13:24 +0900

    Create 구조체.cpp

---
+++ C++/1006/구조체.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+struct Struct {
+private:
+	int H, V;
+public:
+	Struct(int h, int v) { H = h, V = v; }
+	double getArea();
+};
+
+double Struct::getArea() {
+	return H * V;
+}
+
+int main() {
+	Struct 직사각형(4,5);
+	cout << "면적은 " << 직사각형.getArea();
+}
+# 구조체 Struct 학습
